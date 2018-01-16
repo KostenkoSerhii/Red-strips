b92e60d3a418640413d0debead1128b1887b3a91
Serhii <kso_132@mail.ru>
2018-01-16 22:53:19 +0200

    fix form submit

--- src/js/components/formSubmit.js
+++ src/js/components/formSubmit.js
@@ -10,9 +10,10 @@ function formSubmit(){
 			url: m_action,
 			data: m_data,
 			success: function(result){ 
+				$(`.js-popup[data-popup="popup-thanks"]`).fadeIn(300);
+				$("body").addClass("is-hidden");
+				form.find("input, textarea").val("");
 
-				form.reset();
-				openPopup("popup-thanks");
 			}
 		}); 
 	});
